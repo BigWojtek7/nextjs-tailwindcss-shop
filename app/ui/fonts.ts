db8ace6ef1c11bb7a90dca4d928e68d4e4364f92
Wojciech Zagórowski <wojtasjg@icloud.com>
2025-01-19 09:22:37 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
