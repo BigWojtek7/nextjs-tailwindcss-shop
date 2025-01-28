4c9814b58d2b2bf69815842fcb44626cea28500f
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2025-01-28 08:20:28 +0100

    Translate footer text to english

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="footer footer-center bg-base-200 p-10 text-base-content">
       <div>
-        <p>© {new Date().getFullYear()} E-Shop. Wszelkie prawa zastrzeżone.</p>
+        <p>© {new Date().getFullYear()} E-Shop. All rights reserved.</p>
       </div>
     </footer>
   );
